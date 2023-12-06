010265a9eb05f701be7d604e78b5cdd2961d1510
Cuong Nguyen <ntcuong2103@gmail.com>
2023-12-06 18:18:30 +0700

    added tests

--- src/utils.c
+++ src/utils.c
@@ -1,10 +1,8 @@
-#include "sudoku.h"
-
 #include <assert.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-#include "hidden_singles.h"
+#include "sudoku.h"
 
 void init_sudoku(SudokuBoard *p_board)
 {
@@ -221,36 +219,4 @@ void print_candidate_num(SudokuBoard *p_board)
         }
         printf("\n");
     }
-}
-
-int main(int argc, char **argv)
-{
-    if (argc != 2)
-    {
-        printf("Usage: %s <sudoku string>\n", argv[0]);
-        return 1;
-    }
-
-    SudokuBoard *board = malloc(sizeof(SudokuBoard));
-    init_sudoku(board);
-    load_sudoku(board, argv[1]);
-
-    Cell **p_solved_cells = board->solved_cells;
-    int solved_counter = board->solved_counter;
-    while (board->solved_counter < BOARD_SIZE * BOARD_SIZE)
-    {
-        while (show_possible(board, p_solved_cells, solved_counter))
-        {
-            solved_counter = check_solved_cells(board, &p_solved_cells);
-            printf("check_solved_cells %d\n", solved_counter);
-        }
-        // solved_counter = hidden_singles(board, &p_solved_cells);
-        // printf("hidden_singles %d\n", solved_counter);
-    }
-    print_solution(board);
-
-    // clean up
-    free_sudoku(board);
-    free(board);
-    return 0;
 }
